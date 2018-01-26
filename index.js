778fd04260e0f3ac33ef67265d3e088e16547883
Sangeetha Rajeev <sang.rajeev@gmail.com>
2018-01-26 19:16:24 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,25 @@
+function theBeatlesPlay(musicians, instruments) {
+ var myNewArray = [];
+  for(let i = 0; i < musicians.length; i++){
+    myNewArray[i] = musicians[i] + " plays " + instruments[i];
+  }
+  return myNewArray;
+}
+
+function johnLennonFacts(Array) {
+  let i = 0;
+  var myNewArray = [];
+  while (i < Array.length){
+   myNewArray[i] = Array[i] + "!!!";
+   i++;
+  }
+  return myNewArray;
+}
+
+function iLoveTheBeatles (number) {
+  var newArray = [];
+  do {
+    newArray[number] = "I love the Beatles!";
+  } while (number < 15);
+  return newArray;
+}
